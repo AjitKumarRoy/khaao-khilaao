fda6713ba32784d73a2b12503a77fbcc53d78ffc
AjitKumarRoy <ak47riderji@gmail.com>
2024-02-26 22:53:08 +0530

    code updated

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-// Wait for the DOM content to be loaded
 document.addEventListener("DOMContentLoaded", function() {
-    // Function to click the next button
-    function clickNextButton() {
-        // Find the next button
-        const nextButton = document.querySelector('[data-carousel-next]');
+    // Function to click the next button for a specific carousel
+    function clickNextButton(carousel) {
+        // Find the next button within the specified carousel
+        const nextButton = carousel.querySelector('[data-carousel-next]');
         // Trigger a click event on the next button
         nextButton.click();
     }
+
+    // Find all carousels on the page
+    const carousels = document.querySelectorAll('[data-carousel="static"]');
   
-    // Set an interval to click the next button every 3 seconds
-    const intervalId = setInterval(clickNextButton, 3000);
-  });
-  
+    // Set an interval for each carousel to click the next button every 3 seconds
+    carousels.forEach(function(carousel) {
+        setInterval(function() {
+            clickNextButton(carousel);
+        }, 3000);
+    });
+});
